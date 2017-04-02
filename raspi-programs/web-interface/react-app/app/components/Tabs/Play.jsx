4c984e9b14baa5edc1762e956ab5787fa0ed1a4e
Brandon Dube <brandondube@gmail.com>
2017-04-02 15:50:48 -0400

    revised Play component

--- raspi-programs/web-interface/react-app/app/components/Tabs/Play.jsx
+++ raspi-programs/web-interface/react-app/app/components/Tabs/Play.jsx
@@ -1,9 +1,24 @@
 // react
 import React from 'react';
 
+// material-ui
+import * as Colors from 'material-ui/styles/colors';
+
+const containerSty = {
+  textAlign: 'center',
+  background: Colors.indigo800,
+};
+
+const innerContainer = {
+  display: 'inline-block',
+  verticalAlign: 'middle',
+  margin: '0 auto',
+};
 
 const clockSty = {
+  fontSize: 72,
   fontFamily: 'D7CMB',
+  color: Colors.yellow500,
 };
 
 const Play = (props) => {
@@ -11,10 +26,12 @@ const Play = (props) => {
     time,
   } = props;
   return (
-    <div>
-      <h1 style={clockSty}>
-        {time}
-      </h1>
+    <div style={containerSty}>
+      <div style={innerContainer}>
+        <span style={clockSty}>
+          {time}
+        </span>
+      </div>
     </div>
   );
 };
