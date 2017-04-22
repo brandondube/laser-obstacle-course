db3b4f689038e953f00feb8fe93ff961ad011dea
Brandon Dube <brandondube@gmail.com>
2017-04-21 22:27:56 -0400

    basic play buttons

--- raspi-programs/web-interface/react-app/app/components/Tabs/Play.jsx
+++ raspi-programs/web-interface/react-app/app/components/Tabs/Play.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 
 // material-ui
+import RaisedButton from 'material-ui/RaisedButton';
 import * as Colors from 'material-ui/styles/colors';
 
 const containerSty = {
@@ -21,19 +22,68 @@ const clockSty = {
   color: Colors.yellow500,
 };
 
-const Play = (props) => {
-  const {
-    time,
-  } = props;
-  return (
-    <div style={containerSty}>
-      <div style={innerContainer}>
-        <span style={clockSty}>
-          {time}
-        </span>
+class Play extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentButton: 'play',
+    };
+  }
+
+  handlePlayClick = () => {
+    const socket = this.context.socket;
+    socket.emit('game:start');
+    this.setState({
+      currentButton: 'stop',
+    });
+  }
+  handleStopClick = () => {
+    const socket = this.context.socket;
+    socket.emit('game:forcestop');
+    this.setState({
+      currentButton: 'play',
+    });
+  }
+  render() {
+    const { time } = this.props;
+    let button;
+    if (this.state.currentButton === 'play') {
+      button = <RaisedButton
+            label="Play"
+            labelStyle={{
+              fontSize: 48,
+              padding: 48,
+            }}
+            onTouchTap={this.handlePlayClick}
+          />;
+      }
+    else {
+      button = <RaisedButton
+            label="Stop"
+            labelStyle={{
+              fontSize: 48,
+              padding: 48,
+            }}
+            onTouchTap={this.handleStopClick}
+          />
+    }
+    return (
+      <div style={containerSty}>
+        <div style={innerContainer}>
+          {button}
+          <br />
+          <span style={clockSty}>
+            {time}
+          </span>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
+const T = React.PropTypes;
+Play.contextTypes = {
+  socket: T.object,
+}
 export default Play;
